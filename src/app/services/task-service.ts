6eb90a92aa2c6cd76ace09282bd1722ba3c9a7b6
Fabricio Balani <fbalanibarros@hotmail.com>
2026-01-27 08:47:54 -0300

    Implementação TaskService

---
+++ src/app/services/task-service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { Task } from '../models/task';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+
+  private tasks: Array<Task> = [];
+
+  getTasks():Array<Task>{
+
+    const tasks = this.getFromLocalStorage();
+
+    return this.tasks;
+  }
+
+  getById(id: number): Task | undefined {
+    
+    const task = this.tasks.find(c => c.id === id);
+
+    return task;    
+  }
+
+  addtask(task: Task){
+
+    this.tasks.push(task);
+
+    this.saveToLocalStorage();
+  }
+
+  updateTasks(){
+
+    this.saveToLocalStorage();
+  }
+
+  removeTask(task: Task){
+
+    const index = this.tasks.indexOf(task);
+
+    if (index !== -1){
+      
+      //achou
+      this.tasks.splice(index, 1);
+
+      this.saveToLocalStorage();
+    }   
+    
+  }
+
+  private saveToLocalStorage(){
+
+    const tasksJSON = JSON.stringify(this.tasks);
+
+    localStorage.setItem('tasks', tasksJSON);
+  }
+
+  private getFromLocalStorage(): Array<Task>{
+
+    const tasksJSON = localStorage.getItem('tasks');
+
+    if(!tasksJSON){
+
+      return new Array<Task>();
+    }
+
+    return JSON.parse(tasksJSON);
+  }
+}
