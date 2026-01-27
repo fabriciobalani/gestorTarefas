891119a4ffbf89f56e762631bb85b99e15cc303d
Fabricio Balani <fbalanibarros@hotmail.com>
2026-01-27 07:44:24 -0300

    criando componentes e rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { TaskDetails } from './task-details/task-details';
+import { TaskFilter } from './task-filter/task-filter';
+import { TaskList } from './task-list/task-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'taskList', title: 'Lista de Tarefas', component: TaskList},
+    {path: 'taskDetails', title: 'Detalhes da tarefa', component: TaskDetails},    
+    {path:'', redirectTo: '/taskList', pathMatch: 'full'}
+    
+];
+
+
