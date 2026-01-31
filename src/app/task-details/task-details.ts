342757540a98f5769cfa114480bb4e57777b052c
Fabricio Balani <fbalanibarros@hotmail.com>
2026-01-31 10:16:34 -0300

    Implementação do task filter

--- src/app/task-details/task-details.ts
+++ src/app/task-details/task-details.ts
@@ -1,11 +1,57 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Task } from '../models/task';
+import { TaskService } from '../services/task-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-details',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-details.html',
   styleUrl: './task-details.css',
 })
 export class TaskDetails {
+
+  constructor(private router: Router, private route: ActivatedRoute, private taskService: TaskService){}
+
+  task?: Task;
+
+  ngOnInit(){
+
+    let id = this.route.snapshot.paramMap.get('id');
+
+    if(id === null || id === ''){
+
+      this.navigateBack();      
+    }    
+    else{
+
+      this.task = this.taskService.getById(+id);
+
+      if (this.task === undefined){
+
+        this.navigateBack();
+      }
+    }    
+  }
+  
+  save(){
+
+    this.taskService.updateTasks();
+
+    this.navigateBack();
+  }
+
+  cancel(){
+
+    this.navigateBack();
+  }
+  
+  
+  private navigateBack() {
+    
+    this.router.navigate(['/taskList'], {relativeTo: this.route});
+  }
+  
 
 }
